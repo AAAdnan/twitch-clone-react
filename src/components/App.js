6866d11d63dcb3d653256e843fc08e69daa32339
Adnan Ahmed <username@email.com>
2019-06-08 22:56:14 +0800

    added delete function

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import createBrowserHistory from '../history';
 
 
 const App = () => {
-    return <div>
-                <BrowserRouter>
+    return (
+            <div className="ui container">
+                <Router history={createBrowserHistory}>
                 <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new"  component={StreamCreate} />
-                    <Route path="/streams/edit"  component={StreamEdit} />
-                    <Route path="/streams/delete"  component={StreamDelete} />
+                    <Route path="/streams/edit/:id"  component={StreamEdit} />
+                    <Route path="/streams/delete/:id"  component={StreamDelete} />
                     <Route path="/streams/show"  component={StreamShow} />
-                </BrowserRouter>
-            </div>;
+                </Router>
+            </div>
+    ) 
 }
 
 export default App;
